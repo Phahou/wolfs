69f3f7b4be1bd59aad2598d12e047fbe3b7bc02f
Phahou <38053767+Phahou@users.noreply.github.com>
2022-08-05 05:07:39 +0200

    put test_disk into a class

--- test/test_disk.py
+++ test/test_disk.py
@@ -81,202 +81,202 @@ def get_src_cache_directory_pair(tmpdir_factory):
 ###################################################
 # Unit tests
 ###################################################
+class TestDisk:
+	def test_init_disk(self, tmpdir_factory):
+		tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
 
-def test_init_disk(tmpdir_factory):
-	tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
+		# check if everything goes fine
+		disk = prep_Disk(tmpdir_source, tmpdir_cache)
+		assert disk.sourceDir == tmpdir_source
+		assert disk.cacheDir == tmpdir_cache
 
-	# check if everything goes fine
-	disk = prep_Disk(tmpdir_source, tmpdir_cache)
-	assert disk.sourceDir == tmpdir_source
-	assert disk.cacheDir == tmpdir_cache
+		def nodir(faulty_disk_constructor) -> None:
+			with pytest.raises(SystemExit) as pytest_wrapped_e:
+				faulty_disk_constructor()
+			assert pytest_wrapped_e.type == SystemExit
+			assert pytest_wrapped_e.value.code == errno.ENOENT
 
-	def nodir(faulty_disk_constructor) -> None:
-		with pytest.raises(SystemExit) as pytest_wrapped_e:
-			faulty_disk_constructor()
-		assert pytest_wrapped_e.type == SystemExit
-		assert pytest_wrapped_e.value.code == errno.ENOENT
+		# src doesnt exist
+		os.rmdir(tmpdir_source)
+		nodir(lambda: prep_Disk(sourceDir=tmpdir_source, cacheDir=tmpdir_cache))
+		tmpdir_source = tmpdir_factory.mktemp("src")
 
-	# src doesnt exist
-	os.rmdir(tmpdir_source)
-	nodir(lambda: prep_Disk(sourceDir=tmpdir_source, cacheDir=tmpdir_cache))
-	tmpdir_source = tmpdir_factory.mktemp("src")
+		# cache doesnt exist
+		os.rmdir(tmpdir_cache)
+		nodir(lambda: prep_Disk(sourceDir=tmpdir_source, cacheDir=tmpdir_cache))
 
-	# cache doesnt exist
-	os.rmdir(tmpdir_cache)
-	nodir(lambda: prep_Disk(sourceDir=tmpdir_source, cacheDir=tmpdir_cache))
-
-	# both don't exist
-	os.rmdir(tmpdir_source)
-	nodir(lambda: prep_Disk(sourceDir=tmpdir_source, cacheDir=tmpdir_cache))
-
-
-def test_toPathFuncs(tmpdir_factory):
-	tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
-	disk = prep_Disk(tmpdir_source, tmpdir_cache)
-
-	# type str tests
-	root_file = "test.py"
-	# check if a root file is correctly converted
-	assert disk.trans.toSrc(root_file) == Path(f"{tmpdir_source}/{root_file}")
-	assert disk.trans.toTmp(root_file) == Path(f"{tmpdir_cache}/{root_file}")
-	assert disk.trans.toRoot(root_file) == f"/{root_file}"
-
-	subdir = "dir/test.py"
-	# check if a file in a subdirectory works
-	assert disk.trans.toSrc(subdir) == Path(f"{tmpdir_source}/{subdir}")
-	assert disk.trans.toTmp(subdir) == Path(f"{tmpdir_cache}/{subdir}")
-	assert disk.trans.toRoot(subdir) == f"/{subdir}"
-
-	# check if conversion works
-	root_file_p = Path(root_file)
-	assert disk.trans.toSrc(root_file_p) == disk.trans.toSrc(root_file)
-	assert disk.trans.toTmp(root_file_p) == disk.trans.toTmp(root_file)
-	assert disk.trans.toRoot(root_file_p) == disk.trans.toRoot(root_file)
-
-	subdir_p = Path(subdir)
-	assert disk.trans.toSrc(subdir_p) == disk.trans.toSrc(subdir)
-	assert disk.trans.toTmp(subdir_p) == disk.trans.toTmp(subdir)
-	assert disk.trans.toRoot(subdir_p) == disk.trans.toRoot(subdir)
+		# both don't exist
+		os.rmdir(tmpdir_source)
+		nodir(lambda: prep_Disk(sourceDir=tmpdir_source, cacheDir=tmpdir_cache))
 
 
-def test_canStore(tmpdir_factory):
-	tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
-	disk = prep_Disk(tmpdir_source, tmpdir_cache, maxCacheSize=1)
-	DIRECOTRIES_PER_MEGABYTE = int(1024 // 4)  # 256
+	def test_toPathFuncs(self, tmpdir_factory):
+		tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
+		disk = prep_Disk(tmpdir_source, tmpdir_cache)
 
-	# for for normal operation: (directory parents are available)
-	enough: Path = Path(os.path.join(tmpdir_source, name_generator()))
-	for i in range(1, 5):
-		pseudo_file(enough, i * DIRECOTRIES_PER_MEGABYTE)
-		assert disk.canStore(
-			Path(enough)), f"Couldn't store {enough} although there should be enough space  ({i * 25}%)!"
-		os.remove(enough)
+		# type str tests
+		root_file = "test.py"
+		# check if a root file is correctly converted
+		assert disk.trans.toSrc(root_file) == Path(f"{tmpdir_source}/{root_file}")
+		assert disk.trans.toTmp(root_file) == Path(f"{tmpdir_cache}/{root_file}")
+		assert disk.trans.toRoot(root_file) == f"/{root_file}"
 
-	not_enough: Path = Path(os.path.join(tmpdir_source, name_generator()))
-	pseudo_file(not_enough, 5 * DIRECOTRIES_PER_MEGABYTE)
-	assert not disk.canStore(not_enough), f"Could store {not_enough} although there shouldnt be enough space!"
-	os.remove(not_enough)
+		subdir = "dir/test.py"
+		# check if a file in a subdirectory works
+		assert disk.trans.toSrc(subdir) == Path(f"{tmpdir_source}/{subdir}")
+		assert disk.trans.toTmp(subdir) == Path(f"{tmpdir_cache}/{subdir}")
+		assert disk.trans.toRoot(subdir) == f"/{subdir}"
 
-	# for directories which have to be created
-	previous_name: Path = Path(os.path.join(tmpdir_source, name_generator()))
-	alloced_dirs = [previous_name]
-	os.mkdir(previous_name)
-	for i in range(1, 5):
-		subdir_enough: Path = Path(os.path.join(previous_name.__str__() + '/', name_generator()))
-		pseudo_file(subdir_enough, 4 * (DIRECOTRIES_PER_MEGABYTE - i))
-		assert disk.canStore(Path(
-			subdir_enough)), f"Couldn't store {subdir_enough}, although there's enough space even for the needed directories in between!"
-		os.remove(subdir_enough)
-		previous_name = subdir_enough
+		# check if conversion works
+		root_file_p = Path(root_file)
+		assert disk.trans.toSrc(root_file_p) == disk.trans.toSrc(root_file)
+		assert disk.trans.toTmp(root_file_p) == disk.trans.toTmp(root_file)
+		assert disk.trans.toRoot(root_file_p) == disk.trans.toRoot(root_file)
+
+		subdir_p = Path(subdir)
+		assert disk.trans.toSrc(subdir_p) == disk.trans.toSrc(subdir)
+		assert disk.trans.toTmp(subdir_p) == disk.trans.toTmp(subdir)
+		assert disk.trans.toRoot(subdir_p) == disk.trans.toRoot(subdir)
+
+
+	def test_canStore(self, tmpdir_factory):
+		tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
+		disk = prep_Disk(tmpdir_source, tmpdir_cache, maxCacheSize=1)
+		DIRECOTRIES_PER_MEGABYTE = int(1024 // 4)  # 256
+
+		# for for normal operation: (directory parents are available)
+		enough: Path = Path(os.path.join(tmpdir_source, name_generator()))
+		for i in range(1, 5):
+			pseudo_file(enough, i * DIRECOTRIES_PER_MEGABYTE)
+			assert disk.canStore(
+				Path(enough)), f"Couldn't store {enough} although there should be enough space  ({i * 25}%)!"
+			os.remove(enough)
+
+		not_enough: Path = Path(os.path.join(tmpdir_source, name_generator()))
+		pseudo_file(not_enough, 5 * DIRECOTRIES_PER_MEGABYTE)
+		assert not disk.canStore(not_enough), f"Could store {not_enough} although there shouldnt be enough space!"
+		os.remove(not_enough)
+
+		# for directories which have to be created
+		previous_name: Path = Path(os.path.join(tmpdir_source, name_generator()))
+		alloced_dirs = [previous_name]
 		os.mkdir(previous_name)
-		alloced_dirs.append(previous_name)
+		for i in range(1, 5):
+			subdir_enough: Path = Path(os.path.join(previous_name.__str__() + '/', name_generator()))
+			pseudo_file(subdir_enough, 4 * (DIRECOTRIES_PER_MEGABYTE - i))
+			assert disk.canStore(Path(
+				subdir_enough)), f"Couldn't store {subdir_enough}, although there's enough space even for the needed directories in between!"
+			os.remove(subdir_enough)
+			previous_name = subdir_enough
+			os.mkdir(previous_name)
+			alloced_dirs.append(previous_name)
 
-	for d in reversed(alloced_dirs):
-		os.rmdir(d)
+		for d in reversed(alloced_dirs):
+			os.rmdir(d)
 
-	subdir_not_enough_root = os.path.join(tmpdir_source, name_generator())
-	os.mkdir(subdir_not_enough_root)
-	subdir_not_enough: Path = Path(os.path.join(subdir_not_enough_root, name_generator()))
-	pseudo_file(subdir_not_enough, 4 * DIRECOTRIES_PER_MEGABYTE)
-	assert not disk.canStore(subdir_not_enough), f"Could store {not_enough} although there shouldn't be enough space!"
-	os.remove(subdir_not_enough)
-	os.rmdir(subdir_not_enough_root)
+		subdir_not_enough_root = os.path.join(tmpdir_source, name_generator())
+		os.mkdir(subdir_not_enough_root)
+		subdir_not_enough: Path = Path(os.path.join(subdir_not_enough_root, name_generator()))
+		pseudo_file(subdir_not_enough, 4 * DIRECOTRIES_PER_MEGABYTE)
+		assert not disk.canStore(subdir_not_enough), f"Could store {not_enough} although there shouldn't be enough space!"
+		os.remove(subdir_not_enough)
+		os.rmdir(subdir_not_enough_root)
 
 
-def test_copystat(tmpdir_factory):
-	tmpdir_source, _ = get_src_cache_directory_pair(tmpdir_factory)
-	# LATER: also copy xAttrs if there are any
-	src_dir: Path = Path(os.path.join(tmpdir_source, name_generator()))
-	os.mkdir(src_dir)
+	def test_copystat(self, tmpdir_factory):
+		tmpdir_source, _ = get_src_cache_directory_pair(tmpdir_factory)
+		# LATER: also copy xAttrs if there are any
+		src_dir: Path = Path(os.path.join(tmpdir_source, name_generator()))
+		os.mkdir(src_dir)
 
-	src: Path = Path(os.path.join(src_dir, name_generator()))
+		src: Path = Path(os.path.join(src_dir, name_generator()))
 
-	dst: Path = Path(os.path.join(src_dir, name_generator()))
+		dst: Path = Path(os.path.join(src_dir, name_generator()))
 
-	make_test_file(src)
-	nano_sleep()
-	make_test_file(dst)
-
-	Disk.copystat(src, dst)
-	stat_src = os.stat(src)
-	stat_dst = os.stat(dst)
-	for attr in ['st_mode', 'st_dev',
-				 'st_uid', 'st_gid', 'st_size',
-				 'st_atime', 'st_mtime']:
-		assert getattr(stat_src, attr) == getattr(stat_dst, attr), f"Mismatch of copied attribute {attr}"
-	# assert stat_src_dir.st_mode == stat_src.st_mode, f"Mode in {src_dir} wasn't copied from {src}"
-	os.remove(dst)
-	os.remove(src)
-	os.rmdir(src_dir)
-
-@pytest.mark.skip
-def test_mkdir_p(tmpdir_factory):
-	tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
-	disk = prep_Disk(tmpdir_source, tmpdir_cache, maxCacheSize=1)
-
-	# generate a bunch of random nested folders
-	src_dir: Path = Path(os.path.join(tmpdir_source, name_generator()))
-
-	SUBFOLDERS = 2
-	for i in range(SUBFOLDERS - 1):
-		src_dir.mkdir()
+		make_test_file(src)
 		nano_sleep()
-		src_dir: Path = Path(os.path.join(src_dir, name_generator()))
-	nano_sleep()
-	src_dir.mkdir()
+		make_test_file(dst)
 
-	# perform cpdir()
-	actual_size, added_folders = disk.mkdir_p(src_dir)
-	assert len(added_folders) == SUBFOLDERS
-
-	# result should have the same size as original directory
-	expected_size = SUBFOLDERS * EMPTY_DIRECTORY_SIZE
-	assert actual_size == expected_size, f"Actual size and book-keeped size mismatch {actual_size} {expected_size}"
-
-	def check_attrs(s_stat, f_stat):
+		Disk.copystat(src, dst)
+		stat_src = os.stat(src)
+		stat_dst = os.stat(dst)
 		for attr in ['st_mode', 'st_dev',
 					 'st_uid', 'st_gid', 'st_size',
 					 'st_atime', 'st_mtime']:
-			assert getattr(s_stat, attr) == getattr(f_stat, attr), f"{attr} failed for {src_dir} {f}"
+			assert getattr(stat_src, attr) == getattr(stat_dst, attr), f"Mismatch of copied attribute {attr}"
+		# assert stat_src_dir.st_mode == stat_src.st_mode, f"Mode in {src_dir} wasn't copied from {src}"
+		os.remove(dst)
+		os.remove(src)
+		os.rmdir(src_dir)
 
-	# check if directory structure is the same and if st_modes are the same
-	for f in reversed(added_folders):
-		s_stat, f_stat = Path(src_dir).stat(), f.stat()
-		check_attrs(s_stat, f_stat)
-		src_dir = src_dir.parent
+	@pytest.mark.skip
+	def test_mkdir_p(self, tmpdir_factory):
+		tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
+		disk = prep_Disk(tmpdir_source, tmpdir_cache, maxCacheSize=1)
+
+		# generate a bunch of random nested folders
+		src_dir: Path = Path(os.path.join(tmpdir_source, name_generator()))
+
+		SUBFOLDERS = 2
+		for i in range(SUBFOLDERS - 1):
+			src_dir.mkdir()
+			nano_sleep()
+			src_dir: Path = Path(os.path.join(src_dir, name_generator()))
+		nano_sleep()
+		src_dir.mkdir()
+
+		# perform cpdir()
+		actual_size, added_folders = disk.mkdir_p(src_dir)
+		assert len(added_folders) == SUBFOLDERS
+
+		# result should have the same size as original directory
+		expected_size = SUBFOLDERS * EMPTY_DIRECTORY_SIZE
+		assert actual_size == expected_size, f"Actual size and book-keeped size mismatch {actual_size} {expected_size}"
+
+		def check_attrs(s_stat, f_stat):
+			for attr in ['st_mode', 'st_dev',
+						 'st_uid', 'st_gid', 'st_size',
+						 'st_atime', 'st_mtime']:
+				assert getattr(s_stat, attr) == getattr(f_stat, attr), f"{attr} failed for {src_dir} {f}"
+
+		# check if directory structure is the same and if st_modes are the same
+		for f in reversed(added_folders):
+			s_stat, f_stat = Path(src_dir).stat(), f.stat()
+			check_attrs(s_stat, f_stat)
+			src_dir = src_dir.parent
 
 
-def test_getSize():
-	# later when symbolic links are added
-	pass
+	def test_getSize(self):
+		# later when symbolic links are added
+		pass
 
-def test_path2Ino(tmpdir_factory):
-	tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
-	disk = prep_Disk(tmpdir_source, tmpdir_cache, maxCacheSize=1)
-	path: Path = Path(os.path.join(tmpdir_source, name_generator()))
-	rpath = disk.trans.toRoot(path)
+	def test_path2Ino(self, tmpdir_factory):
+		tmpdir_source, tmpdir_cache = get_src_cache_directory_pair(tmpdir_factory)
+		disk = prep_Disk(tmpdir_source, tmpdir_cache, maxCacheSize=1)
+		path: Path = Path(os.path.join(tmpdir_source, name_generator()))
+		rpath = disk.trans.toRoot(path)
 
-	# 1. case: path is unkown -> new ino
-	ino = disk[path]
-	assert disk.trans.path_ino_map.get(rpath) == ino, f"path didnt save ino in internal dict"
+		# 1. case: path is unkown -> new ino
+		ino = disk[path]
+		assert disk.trans.path_ino_map.get(rpath) == ino, f"path didnt save ino in internal dict"
 
-	# 2. case: path is known -> same ino (normal ops)
-	assert disk[path] == ino, f"If known the same ino should be returned"
+		# 2. case: path is known -> same ino (normal ops)
+		assert disk[path] == ino, f"If known the same ino should be returned"
 
-	# 3. case: reusing an ino (in rename ops)
-	del disk.trans[(ino, path.__str__())]
-	ino_rpath = disk.trans.path_ino_map.get(rpath)
-	assert ino_rpath is None, f"{disk.trans.path_ino_map} should contain {path} as it was inserted"
-	__freed_inos = getattr(disk.trans, '_' + disk.trans.__class__.__name__ + '__freed_inos')
-	assert ino in __freed_inos, f"{__freed_inos} should contain {ino} as it was deleted"
+		# 3. case: reusing an ino (in rename ops)
+		del disk.trans[(ino, path.__str__())]
+		ino_rpath = disk.trans.path_ino_map.get(rpath)
+		assert ino_rpath is None, f"{disk.trans.path_ino_map} should contain {path} as it was inserted"
+		__freed_inos = getattr(disk.trans, '_' + disk.trans.__class__.__name__ + '__freed_inos')
+		assert ino in __freed_inos, f"{__freed_inos} should contain {ino} as it was deleted"
 
-	disk.trans.path_to_ino(path, reuse_ino=ino)
-	assert disk.trans.path_ino_map.get(rpath) == ino, f"Should have reused the same ino"
+		disk.trans.path_to_ino(path, reuse_ino=ino)
+		assert disk.trans.path_ino_map.get(rpath) == ino, f"Should have reused the same ino"
 
-	# 4. case: inodes grow only larger
-	path2: Path = Path(os.path.join(tmpdir_source, name_generator()))
-	ino2 = disk[path2]
-	assert ino < ino2, f"The generator of inodes should only generate larger inos"
+		# 4. case: inodes grow only larger
+		path2: Path = Path(os.path.join(tmpdir_source, name_generator()))
+		ino2 = disk[path2]
+		assert ino < ino2, f"The generator of inodes should only generate larger inos"
 
 
 # 5. case: foreign translation update of inos
@@ -286,25 +286,25 @@ def test_path2Ino(tmpdir_factory):
 # Class tests
 ###################################################
 
-def test__cp_path():
-	pass
+	def test__cp_path(self):
+		pass
 
 
-def test_track():
-	pass
+	def test_track(self):
+		pass
 
 
-def test_untrack():
-	pass
+	def test_untrack(self):
+		pass
 
 
-def test_makeRoomForPath():
-	pass
+	def test_makeRoomForPath(self):
+		pass
 
 
-def test_cp2Cache():
-	pass
+	def test_cp2Cache(self):
+		pass
 
 
-def test_rebuildCacheDir():
-	pass
+	def test_rebuildCacheDir(self):
+		pass
